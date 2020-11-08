5bda6bc3f2a717931872cf01a22c9ef1a2ee8e81
Kevin Zhang <a22655168@gmail.com>
2020-11-08 15:37:53 +0800

    add master1

--- Game.java
+++ Game.java
@@ -2,7 +2,7 @@ public class Game {
 	private int rolls[] = new int[21];
 	private int currentRoll = 0;
 	private String dog = 0;
-	private String cat1 = 0;
+	private String cat2 = 0;
 	
 	public void roll(int pins) {
 		rolls[currentRoll++] = pins;
