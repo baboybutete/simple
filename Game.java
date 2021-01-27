2ca3e168a7311d736608f64c873bf899ee7e543f
ACER-USER <ACER-USER@LAPTOP-VBV7Q4R9>
2021-01-27 14:36:59 +0800

    Commit

--- Game.java
+++ Game.java
@@ -1,9 +1,10 @@
 public class Game {
 	private int rolls[] = new int[21];
 	private int currentRoll = 0;
-	private String dog = 0;
-	private String cat2 = 0;
+	private String dog = "0";
+	private String cat2 = "0";
 
+	
 	
 	public void roll(int pins) {
 		rolls[currentRoll++] = pins;
